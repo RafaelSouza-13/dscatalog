ac5b493f949e19aa7faaf341683e764be74156bc
Rafael Souza <rafa.nds13@gmail.com>
2024-07-23 12:02:21 -0300

    Alimentando o banco de dados h2

--- backend/src/main/java/edu/rafael/dscatalog/services/CategoryService.java
+++ backend/src/main/java/edu/rafael/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import edu.rafael.dscatalog.entities.Category;
 import edu.rafael.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
          List<Category> categories = categoryRepository.findAll();
          return categories.stream().map(CategoryDTO::new).toList();
