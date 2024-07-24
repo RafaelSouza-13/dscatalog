572ae2b0589c454479cd481fc599ed73c2577687
Rafael Souza <rafa.nds13@gmail.com>
2024-07-24 12:41:25 -0300

    Refatorando a busca paginada de categoria

--- backend/src/main/java/edu/rafael/dscatalog/services/CategoryService.java
+++ backend/src/main/java/edu/rafael/dscatalog/services/CategoryService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,8 +29,8 @@ public class CategoryService {
     }
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest page){
-        Page<Category> categories = categoryRepository.findAll(page);
+    public Page<CategoryDTO> findAllPaged(Pageable pageable){
+        Page<Category> categories = categoryRepository.findAll(pageable);
         return categories.map(CategoryDTO::new);
     }
 
